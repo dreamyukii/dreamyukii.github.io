5493261de22abe67b68e43e0a472d60ba596f57a
dreamyuki <49896401+dreamyukii@users.noreply.github.com>
2024-09-28 19:30:40 +0700

    fix: Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sekalengrengginang.github.io/',
+	site: 'https://dreamyukii.github.io/',
 	integrations: [mdx(), sitemap()],
 });
